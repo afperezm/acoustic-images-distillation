01df61a51b1277188e31aa22c07e988e354c913e
Andres Felipe Perez Murcia <andres.perez@iit.it>
2019-04-10 22:40:53 +0200

    Fix validation of directory writability

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,8 +6,8 @@ import tensorflow as tf
 def str2dir(dir_name):
     if not os.path.isdir(dir_name):
         raise argparse.ArgumentTypeError('{} is not a directory!'.format(dir_name))
-    elif os.access(dir_name, os.R_OK):
-        return argparse.ArgumentTypeError('{} is not a readable directory!'.format(dir_name))
+    elif os.access(dir_name, os.W_OK) is False:
+        raise argparse.ArgumentTypeError('{} is not a writeable directory!'.format(dir_name))
     else:
         return os.path.abspath(os.path.expanduser(dir_name))
 
